7b53c50f3f15958dadbbceff2a36e30c176e7780
frasdor <frasunkiewicz.dorota@gmail.com>
2024-09-13 14:21:00 +0200

    added gallery and form

--- src/01-gallery.js
+++ src/01-gallery.js
@@ -1,42 +1,21 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import {images} from "./images";
+import {initImages} from "./gallery"
 console.log ({images});
 
-const createImage = ({preview, original, description}) =>{
-    const li = document.createElement("li");
-    li.classList.add("gallery-item");
 
-    li.innerHTML = 
-        `<a class="gallery-link" href="${original}">
-            <img 
-                class="gallery-image" 
-                src="${preview}" 
-                alt="${description}" 
-                />
-         </a>`;
+document.querySelector(".gallery").appendChild(initImages(images));
 
 
-    return li;
-};
-
-const initImages = (images) => {
-   const imgs =  images.map(createImage);
-   const fragment = document.createDocumentFragment();
-   fragment.append(...imgs);
-   return fragment;
-};
-
-const main = () => {
-    document.querySelector(".gallery").appendChild(initImages(images));
-};
-
-main();
-
-const lightbox = new SimpleLightbox('ul.gallery li', {
-     captionType: "alt"
+const lightbox = new SimpleLightbox('ul.gallery li a', {
+     captionType: "alt",
+     sourceAttr: "href",
+     overlay: true,
  });
 
 
-    
+    lightbox.options('show.simplelightbox', function() {
+        console.log("lightbox on")
+    });
     
---
+++ src/02-form.js
@@ -0,0 +1,3 @@
+
+const STORAGE_KEY = 'feedback-form-state';
+
